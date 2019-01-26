06714664083741f5b7c67d54162e3d1300e315f1
= <lovbra00@gmail.com>
2019-01-26 15:53:11 +0100

    fck main

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::boxed::Box;
-
-fn main() {
-    let mut val = Box::new(5);
-    let raw = val.as_mut() as *mut i32;
-    let box_raw = Box::into_raw(val);
-    dbg!(raw);
-    dbg!(box_raw);
-}
